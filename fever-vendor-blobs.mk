d730069c817326929565759f4fb863bec13ec607
DerTeufel <dominik-kassel@gmx.de>
2018-02-02 13:34:28 +0100

    init: files pulled from stock rom

---
+++ fever-vendor-blobs.mk
@@ -0,0 +1,3 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/fever/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/fever/vendor,system/vendor)
---
+++ fever-vendor.mk
@@ -0,0 +1,1 @@
+$(call inherit-product, vendor/wiko/fever/fever-vendor-blobs.mk)
